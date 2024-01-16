f5730265c79b00126f79614fec2ac1c9d186eb1c
Diaz027 <dami27cito@gmail.com>
2024-01-16 16:28:16 -0300

    NavBar

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,3 +1,9 @@
+import NavBar from "@/componentes/NavBar/NavBar";
+
 export default function Home() {
-  return;
+  return (
+    <>
+      <NavBar />
+    </>
+  );
 }
---
+++ my-app/src/componentes/NavBar/NavBar.jsx
@@ -0,0 +1,5 @@
+const NavBar = () => {
+  return <h1>esta es la navbar</h1>;
+};
+
+export default NavBar;
