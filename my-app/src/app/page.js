dae94a14e0a688581f31abce6e8d99eb0bf97005
Diaz027 <dami27cito@gmail.com>
2024-01-18 15:39:11 -0300

    contacto

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -5,12 +5,14 @@ import NavBarMobile from "@/componentes/NavBar/NavBarMobile";
 import "./pages.css";
 import LandingPages from "@/componentes/landing/landingPages";
 import Vorttex from "@/componentes/Vorttex/Vorttex";
+import Email from "@/componentes/Contacto/Contac";
 
 export default function Home() {
   return (
     <div className="container">
       <LandingPages />
-      <Vorttex id="proyecto" ></Vorttex>
+      <Vorttex id="proyecto"></Vorttex>
+      <Email id="contacto" />
     </div>
   );
 }
