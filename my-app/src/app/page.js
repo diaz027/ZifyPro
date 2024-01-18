83b34d383e904ce41d540ea51db0f323d7026cce
Diaz027 <dami27cito@gmail.com>
2024-01-18 17:54:21 -0300

    scrollW

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,18 +1,24 @@
 "use client";
-import React, { useState, useEffect } from "react";
 import "./pages.css";
 import LandingPages from "@/componentes/landing/landingPages";
 import Vorttex from "@/componentes/Vorttex/Vorttex";
 import Email from "@/componentes/Contacto/Contac";
 import NavBB from "@/componentes/navBB/NavBB";
+import { Element } from "react-scroll";
 
 export default function Home() {
   return (
     <div className="container">
-      <NavBB/>
-      <LandingPages />
-      <Vorttex id="proyecto"></Vorttex>
-      <Email id="contacto" />
+      <NavBB />
+      <Element name="inicio">
+        <LandingPages />
+      </Element>
+      <Element name="proyecto">
+        <Vorttex />
+      </Element>
+      <Element name="contacto">
+        <Email />
+      </Element>
     </div>
   );
 }
--- my-app/src/componentes/NavBar/NavBarMobile.jsx
+++ my-app/src/componentes/NavBar/NavBarMobile.jsx
@@ -7,50 +7,55 @@ import {
   NavbarMenu,
   NavbarContent,
   NavbarItem,
-  Link,
 } from "@nextui-org/react";
+import { Link as ScrollLink } from "react-scroll";
 
 export default function NavBarMobile() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
   const menuItems = [
-    { name: "Inicio", link: "/" },
-    { name: "Proyectos", link: "#proyecto" },
-    { name: "Nosotros", link: "/about" },
-    { name: "Trabajemos", link: "#contacto" },
+    { name: "Inicio", link: "inicio" },
+    { name: "Proyectos", link: "proyecto" },
+    { name: "Nosotros", link: "nosotros" },
+    { name: "Trabajemos", link: "contacto" },
   ];
 
+  const handleItemClick = () => {
+    setIsMenuOpen(false);
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(isMenuOpen);
+  };
+
   return (
-
-
-    <Navbar  isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
+    <Navbar isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
       <NavbarContent className="sm:hidden" justify="start">
         <NavbarMenuToggle
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          onClick={toggleMenu}
         />
       </NavbarContent>
 
       <NavbarContent className="hidden sm:flex pr-3" justify="center">
         {menuItems.map((item, index) => (
-          <NavbarItem key={`${item.name}-${index}`}>
-            <Link color="foreground" href={item.link}>
-              {item.name}
-            </Link>
+          <NavbarItem key={`${item.name}-${index}`} onClick={handleItemClick}>
+            <ScrollLink to={item.link} smooth={true} onClick={handleItemClick}>
+              <a>{item.name}</a>
+            </ScrollLink>
           </NavbarItem>
         ))}
       </NavbarContent>
 
       <NavbarMenu className="sm:hidden">
         {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item.name}-${index}`}>
-            <Link
-              className="w-full"
-              color="foreground"
-              href={item.link}
-              size="lg"
-              >
-              {item.name}
-            </Link>
+          <NavbarMenuItem
+            key={`${item.name}-${index}`}
+            onClick={handleItemClick}
+          >
+            <ScrollLink to={item.link} smooth={true} onClick={handleItemClick}>
+              <a>{item.name}</a>
+            </ScrollLink>
           </NavbarMenuItem>
         ))}
       </NavbarMenu>
