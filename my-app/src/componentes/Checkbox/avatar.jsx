4fef03eabb324af2b18bf6abf39966534c0fdf50
Diaz027 <dami27cito@gmail.com>
2024-01-19 13:32:08 -0300

    about css

---
+++ my-app/src/componentes/Checkbox/avatar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { CustomUserCard } from "./CustomCheckbox";
+
+export default function Avatar() {
+  return (
+    <div className="flex flex-col gap-1 w-full">
+      <CustomUserCard
+        user={{
+          name: "Damian ",
+          avatar: "./dami.jpeg",
+          username: "Diaz",
+          url: "https://www.instagram.com/damicitoo/",
+          role: "Full Stack Developer",
+          status: "Active",
+        }}
+        statusColor="secondary"
+      />
+      <CustomUserCard
+        user={{
+          name: "Jhonatan",
+          avatar: "./jona.png",
+          username: "Gabriel",
+          url: "https://www.instagram.com/vestigio_sincero/",
+          role: "Full Stack Developer",
+          status: "Active",
+        }}
+        statusColor="warning"
+      />
+    </div>
+  );
+}
