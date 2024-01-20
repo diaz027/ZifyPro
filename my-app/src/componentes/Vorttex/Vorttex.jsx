234a0f5ebbaa3098aeb4edc610382039cf711684
Gabriel J. <106175930+zuoki@users.noreply.github.com>
2024-01-20 16:35:36 -0300

    trabajos

--- my-app/src/componentes/Vorttex/Vorttex.jsx
+++ my-app/src/componentes/Vorttex/Vorttex.jsx
@@ -1,27 +1,81 @@
 "use client";
-import "./vorttex.css";
-import Image from "next/image";
-import vorttex00 from "./vorttex00.jpg";
-import vorttex1 from "./vorttex1.jpg";
-import vorttex2 from "./vorttex2.jpg";
-import vorttex3 from "./vorttex3.jpg";
-import vorttex4 from "./vorttex4.jpg";
 
-const images = [vorttex00, vorttex1, vorttex2, vorttex3, vorttex4];
 
+import React, { useEffect, useRef } from "react";
+import {Image} from "@nextui-org/react";
+import "./vorttex.scss";
 const Vorttex = () => {
+  const titleRef = useRef(); // Crear la referencia
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        // Aquí puedes agregar o quitar la clase según sea visible o no
+        if (entry.isIntersecting) {
+          entry.target.classList.add("wobble-horizontal-top");
+        } else {
+          entry.target.classList.remove("wobble-horizontal-top");
+        }
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.1
+      }
+    );
+
+    if (titleRef.current) {
+      observer.observe(titleRef.current);
+    }
+
+    return () => {
+      if (titleRef.current) {
+        observer.unobserve(titleRef.current);
+      }
+    };
+  }, []);
+
+
+  window.addEventListener('scroll',function(){
+
+   var imagenm= document.getElementById("Image");
+    var altura=window.innerHeight/1.45;
+    console.log(altura)
+    var distancia= imagenm.getBoundingClientRect().top;
+    console.log(distancia)
+
+    if(distancia <= altura){
+      imagenm.classList.add('aparece','transform_left')
+    } else{
+      imagenm.classList.remove('aparece')
+    }
 
 
 
+  })
 
   return (
     <section className="proyecto" id="proyecto">
-      <div className="tittleTrabajos">
-         <h1> Trabajos</h1>
+      <div className="tittleTrabajos" >
+         <h1 ref={titleRef}> Trabajos</h1> {/* Usar la referencia aquí */}
       </div>
+      <img  id="Image" src="/img/vorttex00.jpg" alt="" />
+      <div>
+        <h1 className="pepe">E-comerce</h1>
+      </div>
+      < div className="pepe2">
+          dedicada a la venta de keys online, vortex gaming<br/> es una plataforma profecional
+          a gran escala<br/> con diferentes implementaciones:
 
-
+          <ol>
+            <li>autenticacion de terceros</li>
+            <li>panel de administrador</li>
+            <li>gestion de stock</li>
+            <li>DB/API propia </li>
+          </ol>
+      </div>
       </section>
   );
 };
+
 export default Vorttex;
