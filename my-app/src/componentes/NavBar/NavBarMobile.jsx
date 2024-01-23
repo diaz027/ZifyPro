639d77b2860a3ee703abe207e21aae966e88c3af
Diaz027 <dami27cito@gmail.com>
2024-01-22 21:35:52 -0300

    mobile

--- my-app/src/componentes/NavBar/NavBarMobile.jsx
+++ my-app/src/componentes/NavBar/NavBarMobile.jsx
@@ -1,3 +1,67 @@
+// import "./Navbar.css";
+// import React from "react";
+// import {
+//   Navbar,
+//   NavbarMenuToggle,
+//   NavbarMenuItem,
+//   NavbarMenu,
+//   NavbarContent,
+//   NavbarItem,
+// } from "@nextui-org/react";
+// import { Link as ScrollLink } from "react-scroll";
+
+// export default function NavBarMobile() {
+//   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+//   const menuItems = [
+//     { name: "Inicio", link: "inicio" },
+//     { name: "Proyectos", link: "proyecto" },
+//     { name: "Nosotros", link: "nosotros" },
+//     { name: "Trabajemos", link: "contacto" },
+//   ];
+
+//   const handleItemClick = () => {
+//     setIsMenuOpen(false);
+//   };
+
+//   const toggleMenu = () => {
+//     setIsMenuOpen(isMenuOpen);
+//   };
+
+//   return (
+//     <Navbar isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
+//       <NavbarContent className="sm:hidden" justify="start">
+//         <NavbarMenuToggle
+//           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+//           onClick={toggleMenu}
+//         />
+//       </NavbarContent>
+
+//       <NavbarContent className="hidden sm:flex pr-3" justify="center">
+//         {menuItems.map((item, index) => (
+//           <NavbarItem key={`${item.name}-${index}`} onClick={handleItemClick}>
+//             <ScrollLink to={item.link} smooth={true} onClick={handleItemClick}>
+//               <a>{item.name}</a>
+//             </ScrollLink>
+//           </NavbarItem>
+//         ))}
+//       </NavbarContent>
+
+//       <NavbarMenu className="sm:hidden">
+//         {menuItems.map((item, index) => (
+//           <NavbarMenuItem
+//             key={`${item.name}-${index}`}
+//             onClick={handleItemClick}
+//           >
+//             <ScrollLink to={item.link} smooth={true} onClick={handleItemClick}>
+//               <a>{item.name}</a>
+//             </ScrollLink>
+//           </NavbarMenuItem>
+//         ))}
+//       </NavbarMenu>
+//     </Navbar>
+//   );
+// }
 import "./Navbar.css";
 import React from "react";
 import {
@@ -8,16 +72,16 @@ import {
   NavbarContent,
   NavbarItem,
 } from "@nextui-org/react";
-import { Link as ScrollLink } from "react-scroll";
+import Link from "next/link";
 
 export default function NavBarMobile() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
   const menuItems = [
-    { name: "Inicio", link: "inicio" },
-    { name: "Proyectos", link: "proyecto" },
-    { name: "Nosotros", link: "nosotros" },
-    { name: "Trabajemos", link: "contacto" },
+    { name: "Inicio", link: "/" },
+    { name: "Proyectos", link: "/#proyecto" },
+    { name: "Nosotros", link: "/about" },
+    { name: "Trabajemos", link: "/#contacto" },
   ];
 
   const handleItemClick = () => {
@@ -40,9 +104,7 @@ export default function NavBarMobile() {
       <NavbarContent className="hidden sm:flex pr-3" justify="center">
         {menuItems.map((item, index) => (
           <NavbarItem key={`${item.name}-${index}`} onClick={handleItemClick}>
-            <ScrollLink to={item.link} smooth={true} onClick={handleItemClick}>
-              <a>{item.name}</a>
-            </ScrollLink>
+            <Link href={item.link}>{item.name}</Link>
           </NavbarItem>
         ))}
       </NavbarContent>
@@ -53,9 +115,7 @@ export default function NavBarMobile() {
             key={`${item.name}-${index}`}
             onClick={handleItemClick}
           >
-            <ScrollLink to={item.link} smooth={true} onClick={handleItemClick}>
-              <a>{item.name}</a>
-            </ScrollLink>
+            <Link href={item.link}>{item.name}</Link>
           </NavbarMenuItem>
         ))}
       </NavbarMenu>
--- my-app/src/componentes/model/model.jsx
+++ my-app/src/componentes/model/model.jsx
@@ -1,84 +1,95 @@
-// import { Canvas } from "@react-three/fiber";
-// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-// import { Suspense, useEffect, useState } from "react";
-// import "./model.css";
-
-// function Model() {
-//   const [model, setModel] = useState();
-
-//   useEffect(() => {
-//     new GLTFLoader().load("./sceneFinalisima.gltf", setModel);
-//   }, []);
-
-//   return model ? <primitive object={model.scene} /> : null;
-// }
-
-// export default function MyModel() {
-//   return (
-//     <Canvas className="canvas" style={{ width: "150%", height: "150%" }}>
-//       <Suspense fallback={null}>
-//         <Model />
-//       </Suspense>
-//     </Canvas>
-//   );
-// }
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { Suspense, useEffect, useState, useRef } from "react";
+import { Suspense, useEffect, useState } from "react";
 import "./model.css";
 
 function Model() {
   const [model, setModel] = useState();
-  const modelRef = useRef();
-
-  // Guarda la posición y rotación iniciales
-  var initialPosition = new THREE.Vector3();
+  var initialPosition = new THREE.Vector3(-2, -2, 0);
   var initialRotation = new THREE.Euler();
 
-  function pointermove(event) {
-    // Calcula los nuevos valores de posición y rotación
-    var newX = (event.clientX / window.innerWidth) * -2;
-    var newY = (event.clientY / window.innerHeight) * 1;
-    var newRotX = (event.clientY / window.innerHeight) * 1;
-    var newRotY = (event.clientX / window.innerWidth) * -1;
-
-    // Limita los valores a un máximo de 15
-    newX = Math.min(newX, 15);
-    newY = Math.min(newY, 15);
-    newRotX = Math.min(newRotX, 15);
-    newRotY = Math.min(newRotY, 15);
-
-    // Aplica los nuevos valores
-    modelRef.current.position.x = initialPosition.x + newX;
-    modelRef.current.position.y = initialPosition.y + newY;
-    modelRef.current.rotation.x = initialRotation.x + newRotX;
-    modelRef.current.rotation.y = initialRotation.y + newRotY;
-  }
-
   useEffect(() => {
-    new GLTFLoader().load("./sceneFinalisima.gltf", setModel);
+    new GLTFLoader().load("./sceneFinalisima.gltf", (gltf) => {
+      gltf.scene.position.copy(initialPosition);
+      gltf.scene.rotation.copy(initialRotation);
+      setModel(gltf);
+    });
   }, []);
 
-  useEffect(() => {
-    if (modelRef.current) {
-      window.addEventListener("mousemove", pointermove);
-    }
-
-    return () => {
-      window.removeEventListener("mousemove", pointermove);
-    };
-  }, [model]);
-
-  return model ? <primitive object={model.scene} ref={modelRef} /> : null;
+  return model ? <primitive object={model.scene} /> : null;
 }
 
 export default function MyModel() {
   return (
-    <Canvas className="canvas" style={{ width: "150%", height: "150%" }}>
+    <Canvas className="canvas" style={{ width: "80vh", height: "80vh" }}>
       <Suspense fallback={null}>
         <Model />
       </Suspense>
     </Canvas>
   );
 }
+
+// import * as THREE from "three";
+// import { Canvas } from "@react-three/fiber";
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+// import { Suspense, useEffect, useState, useRef } from "react";
+// import "./model.css";
+
+// function Model() {
+//   const [model, setModel] = useState();
+//   const modelRef = useRef();
+
+//   // Guarda la posición y rotación iniciales
+//   var initialPosition = new THREE.Vector3(-3, -2, 0); // Cambia el valor de x a -2
+//   var initialRotation = new THREE.Euler();
+
+//   function pointermove(event) {
+//     // Calcula los nuevos valores de posición y rotación
+//     var newX = (event.clientX / window.innerWidth) * -2;
+//     var newY = (event.clientY / window.innerHeight) * 1;
+//     var newRotX = (event.clientY / window.innerHeight) * 1;
+//     var newRotY = (event.clientX / window.innerWidth) * -1;
+
+//     // Limita los valores a un máximo de 15
+//     newX = Math.min(newX, 15);
+//     newY = Math.min(newY, 15);
+//     newRotX = Math.min(newRotX, 15);
+//     newRotY = Math.min(newRotY, 15);
+
+//     // Aplica los nuevos valores
+//     modelRef.current.position.x = initialPosition.x + newX;
+//     modelRef.current.position.y = initialPosition.y + newY;
+//     modelRef.current.rotation.x = initialRotation.x + newRotX;
+//     modelRef.current.rotation.y = initialRotation.y + newRotY;
+//   }
+
+//   useEffect(() => {
+//     new GLTFLoader().load("./sceneFinalisima.gltf", (gltf) => {
+//       setModel(gltf);
+//       gltf.scene.scale.set(1.5, 1.5, 1.5); // Aumenta el tamaño del modelo en un 50%
+//     });
+//   }, []);
+
+//   useEffect(() => {
+//     if (modelRef.current) {
+//       window.addEventListener("mousemove", pointermove);
+//     }
+
+//     return () => {
+//       window.removeEventListener("mousemove", pointermove);
+//     };
+//   }, [model]);
+
+//   return model ? <primitive object={model.scene} ref={modelRef} /> : null;
+// }
+
+// export default function MyModel() {
+//   return (
+//     <Canvas className="canvas" style={{ width: "70vh", height: "70vh" }}>
+//       <Suspense fallback={null}>
+//         <Model />
+//       </Suspense>
+//     </Canvas>
+//   );
+// }
